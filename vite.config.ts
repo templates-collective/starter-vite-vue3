04d08cdf90d61fc0124c046c6ebedc6e1f87e9f7
kaivanwong <kaivanwong@outlook.com>
2023-12-05 17:31:49 +0800

    feat: add env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { URL, fileURLToPath } from 'node:url'
+import { type ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default (options: ConfigEnv) => {
+  const { mode } = options
+
+  const envDir = '.vite/env/'
+
+  const env = loadEnv(mode, envDir, ['VITE_', 'APP_', 'AXIOS_'])
+
+  return defineConfig({
+    base: env.VITE_BASE_URL,
+    envDir,
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  })
+}
