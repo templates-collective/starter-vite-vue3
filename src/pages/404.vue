3a74510bf5d2c60739d25e8de4f8f6a21f1a641e
kaivanwong <kaivanwong@outlook.com>
2024-01-22 16:45:46 +0800

    feat: add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const router = useRouter()
+const { t } = useI18n()
+</script>
+
+<template>
+  <main p="x4 y10" text="center teal-700 dark:gray-200">
+    <div text-4xl>
+      <i i-carbon-warning inline-block />
+    </div>
+    <RouterView />
+    <div>
+      <Button is="vue:Button" @click="router.back()">
+        {{ t('button.back') }}
+      </Button>
+    </div>
+  </main>
+</template>
