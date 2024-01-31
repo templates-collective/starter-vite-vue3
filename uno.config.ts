444b21cf2be6cdf573df83aa789cf042a9f7fa30
kaivanwong <kaivanwong@outlook.com>
2024-01-31 14:59:49 +0800

    feat: update bg color

--- uno.config.ts
+++ uno.config.ts
@@ -12,6 +12,11 @@ import {
 // UnoCSS config.
 // https://github.com/unocss/unocss
 export default defineConfig({
+  theme: {
+    'light-color': '#f2f1ec',
+    'dark-color': '#171717',
+    'primary-color': '#15803d',
+  },
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-green-600 text-white cursor-pointer hover:bg-green-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-green-600'],
