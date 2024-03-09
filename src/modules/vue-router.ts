856aaf1b16d8ca8cc5d7d2002cec83948e890f0f
kaivanwong <kaivanwong@outlook.com>
2024-03-09 09:37:06 +0800

    feat: add nprogress for router

--- src/modules/vue-router.ts
+++ src/modules/vue-router.ts
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes as autoRoutes } from 'vue-router/auto/routes'
+import NProgress from 'nprogress'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: () => {
     return setupLayouts(autoRoutes)
   },
+})
+
+router.beforeEach((to, from) => {
+  if (to.path !== from.path)
+    NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
