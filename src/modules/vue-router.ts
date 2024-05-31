ef3d4574b92f7235315b1115df9ca06ab1b07935
kevinwong <kevinwong865@gmail.com>
2024-05-31 15:37:34 +0800

    fix: fix env

--- src/modules/vue-router.ts
+++ src/modules/vue-router.ts
@@ -4,7 +4,7 @@ import { routes as autoRoutes } from 'vue-router/auto/routes'
 import NProgress from 'nprogress'
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: () => {
     return setupLayouts(autoRoutes)
   },
