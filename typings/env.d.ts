0ab37ef28e61cd795eb0aaf8cf0ff592a852aaf4
kevinwong <kevinwong865@gmail.com>
2024-06-05 11:46:59 +0800

    feat(types): add env types

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -2,11 +2,12 @@
 /// <reference types="vite-plugin-pages/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_ENV_MODE: string
   readonly VITE_BASE_URL: string
-  readonly FETCH_URL: string
-  readonly FETCH_URL_PREFIX: string
-  readonly APP_CONSOLE_OUTPUT: boolean
+  readonly VITE_APP_DEFAULT_TITLE: string
+  readonly VITE_APP_REQUEST_URL: string
+  readonly VITE_APP_REQUEST_PROXY_URL: string
+  readonly VITE_APP_SOCKET_URL: string
+  readonly VITE_APP_SOCKET_PROXY_URL: string
 }
 
 interface ImportMeta {
