fb8f39f41d567f4cea42d5f4f45d5442381fb7c1
kieranwong9865 <kieranwong9865@gmail.com>
2024-08-27 11:45:41 +0800

    feat: add error interceptor for axios

--- src/composables/request.ts
+++ src/composables/request.ts
@@ -1,4 +1,4 @@
-import type { AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import type { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import axios from 'axios'
 import { assign } from 'lodash-es'
 
@@ -12,6 +12,7 @@ import { assign } from 'lodash-es'
 export function useRequest(config?: AxiosRequestConfig, interceptors?: {
   request?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig
   response?: (config: AxiosResponse) => AxiosResponse
+  responseError?: (error: AxiosError) => AxiosError
 }) {
   const request = axios.create(assign({
     baseURL: import.meta.env.VITE_APP_REQUEST_URL,
@@ -25,20 +26,26 @@ export function useRequest(config?: AxiosRequestConfig, interceptors?: {
     const timestamp = new Date().getTime()
     config.headers['X-Timestamp'] = timestamp
     config.params = assign({}, config.params, { _t: timestamp })
-
+    // todo something...
     return config
   }
 
   const interceptorsResponse = (response: AxiosResponse) => {
-    if (response.status === 200) {
-      return response
+    // todo something...
+    return response
+  }
+
+  const interceptorsResponseError = (error: AxiosError) => {
+    if (!error.response) {
+      return error
     }
-    return Promise.reject(response)
+    // todo something...
+    return error
   }
 
   request.interceptors.request.use(interceptors?.request || interceptorsRequest)
 
-  request.interceptors.response.use(interceptors?.response || interceptorsResponse)
+  request.interceptors.response.use(interceptors?.response || interceptorsResponse, interceptors?.responseError || interceptorsResponseError)
 
   return {
     request,
