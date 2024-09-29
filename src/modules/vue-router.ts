c1a15fce46e5e69a9d79aefa2b15b4cf1053d5b5
kieranwong9865 <kieranwong9865@gmail.com>
2024-09-29 17:20:38 +0800

    feat: update router base url with env

--- src/modules/vue-router.ts
+++ src/modules/vue-router.ts
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes as autoRoutes, handleHotUpdate } from 'vue-router/auto-routes'
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_BASE_URL),
   routes: setupLayouts(autoRoutes),
 })
 
